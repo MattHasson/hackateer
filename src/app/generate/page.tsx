8a712b4882ed75a18fecf74892fb3ce60cea7a24
Mumtahin Farabi <mfarabi619@gmail.com>
2023-07-22 18:48:11 -0400

    feat: Style input boxes in generate page

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,31 +1,31 @@
 /* eslint-disable react/no-unescaped-entities */
 export default function Generate() {
   return (
-    <main className="min-h-screen flex flex-col mt-[50px]">
+    <main className="min-h-screen flex flex-col mt-[50px] space-y-4 mr-20">
       <h2 className="font-extrabold text-3xl md:text-4xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-br from-[#0148FF] to-[#00F0FF] leading-none">
-      Problem Statement
+        Problem Statement
       </h2>
-            <input className="text-xs font-bold md:text-lg lg:text-2xl text-majesticOrchid"></input>
-      <h2 className="font-extrabold text-3xl md:text-4xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-br from-[#0148FF] to-[#00F0FF] leading-none">
-      Hackathon Duration
+      <input className="text-xs md:text-md lg:text-xl text-raspberryFizz bg-mysticPurple rounded-lg"></input>
+      <h2 className="pt-5 font-extrabold text-3xl md:text-4xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-br from-[#0148FF] to-[#00F0FF] leading-none">
+        Hackathon Duration
       </h2>
-            <input className="text-xs font-bold md:text-lg lg:text-2xl text-majesticOrchid"></input>
+      <input className="text-xs md:text-md lg:text-xl text-raspberryFizz bg-mysticPurple rounded-lg"></input>
 
-
-      <div className="grid grid-cols-2">
+      <div className="pt-5 grid grid-cols-2">
         <div>
-
-      <h4 className="font-extrabold text-lg md:text-8xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-br from-[#0148FF] to-[#00F0FF] leading-none">
-        Team member(s)
+          <h4 className="font-extrabold text-md md:text-xl lg:text-3xl bg-clip-text text-transparent bg-gradient-to-br from-[#0148FF] to-[#00F0FF] leading-none pb-2 pt-4">
+            Team member(s)
           </h4>
-      <input className="text-xs font-bold md:text-lg lg:text-2xl text-majesticOrchid"></input>
+          <input className="text-xs md:text-md lg:text-xl text-raspberryFizz bg-mysticPurple rounded-lg"></input>
         </div>
-          <div>
-      <h4 className="font-extrabold text-lg md:text-8xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-br from-[#0148FF] to-[#00F0FF] leading-none">Skills</h4>
-            <input className="text-xs font-bold md:text-lg lg:text-2xl text-majesticOrchid"></input>
-          </div>
+        <div>
+          <h4 className="font-extrabold text-md md:text-xl lg:text-3xl bg-clip-text text-transparent bg-gradient-to-br from-[#0148FF] to-[#00F0FF] leading-none pb-2 pt-4">
+            Skills
+          </h4>
+          <input className="text-xs md:text-md lg:text-xl text-raspberryFizz bg-mysticPurple rounded-lg"></input>
+        </div>
       </div>
-      <button className="text-sm md:text-lg lg:text-2xl px-4 py-1 lg:px-6 mt-2 transition duration-500 ease-in-out border-2 rounded-md text-raspberryFizz border-raspberryFizz hover:bg-raspberryFizz hover:text-moonlitIvory">
+      <button className="text-sm md:text-lg lg:text-2xl px-4 py-1 lg:px-6 mt-2 transition duration-500 ease-in-out border-2 rounded-lg text-raspberryFizz border-raspberryFizz hover:bg-raspberryFizz hover:text-moonlitIvory">
         Get Ideas 🥳
       </button>
     </main>

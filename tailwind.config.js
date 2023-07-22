e5b081d583557712eba15a59c8081b7718664c44
Mumtahin Farabi <mfarabi619@gmail.com>
2023-07-22 08:33:47 -0400

    chore: Add colors palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,23 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        // 'gradient-conic':
+        //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        midnightNavy: '##0F0B38',
+        aquamarineDream: '#23C6DC',
+        electricCitrus: '#FFA500',
+        forestGreen: '#00A600',
+        enchantedEmerald: '#00FF00',
+        berryBlush: '#BE397C',
+        raspberryFizz: '#FF69B4',
+        moonlitIvory: '#F0F0F0',
+        charcoalMist: '#333333',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
       },
     },
   },
